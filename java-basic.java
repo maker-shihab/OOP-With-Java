d687b4d73b6cecf65a38e10ef8fb3e0d69387e26
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-09-24 18:12:45 +0600

    count frequency of each element

--- java-basic.java
+++ java-basic.java
@@ -262,3 +262,43 @@ public class ArrayDeletionExample {
   Contents of newArray: 1 2 4 5 
 */
 
+// 11.Write a Java program to count frequency of each element in an array.
+
+public class Frequency {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 8, 3, 2, 2, 2, 5, 1};
+        int[] fr = new int[arr.length];
+        int visited = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    fr[j] = visited;
+                }
+            }
+            if (fr[i] != visited)
+                fr[i] = count;
+        }
+
+        System.out.println("---------------------------------------");
+        System.out.println(" Element | Frequency");
+        System.out.println("---------------------------------------");
+        for (int i = 0; i < fr.length; i++) {
+            if (fr[i] != visited)
+                System.out.println("    " + arr[i] + "    |    " + fr[i]);
+        }
+        System.out.println("----------------------------------------");
+    }
+}
+/*
+-----------------------
+| Element | Frequency |
+-----------------------
+    1    |    2
+    2    |    4
+    8    |    1
+    3    |    1
+    5    |    1
+-----------------------
+*/
